558577f9075bf118236017d1f37191183c968760
Camilo López <camiloj.lopez79@gmail.com>
2024-05-29 12:43:32 -0400

    React Navigation - Stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import 'react-native-gesture-handler';
+
 // React Native
 import {
   Text,
-  View
 } from 'react-native';
 // React Native Natigation
 import { NavigationContainer } from '@react-navigation/native';
+// Routes
+import { StackNavigator } from './presentation/routes';
 
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Text>App.tsx</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
