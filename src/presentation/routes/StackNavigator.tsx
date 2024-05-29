7df84b4d387f76b8efa004981de6b410a038c70a
Camilo López <camiloj.lopez79@gmail.com>
2024-05-29 13:54:12 -0400

    FlatList - Pantalla de productos

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Screens
 import {
   HomeScreen,
+  ProductScreen,
   ProductsScreen,
   SettingsScreen
 } from '../screens';
@@ -21,6 +22,7 @@ export const StackNavigator = () => {
       }}
     >
       <Stack.Screen name='Home' component={ HomeScreen } />
+      <Stack.Screen name='Product' component={ ProductScreen } />
       <Stack.Screen name='Products' component={ ProductsScreen } />
       <Stack.Screen name='Settings' component={ SettingsScreen } />
     </Stack.Navigator>
