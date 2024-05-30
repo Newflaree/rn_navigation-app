54bba8fd1396725eb29890bf3e1f75af902c8296
Camilo López <camiloj.lopez79@gmail.com>
2024-05-30 09:50:12 -0400

    Toggle Drawer - Mostrar / Ocultar

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,14 @@
+// React
+import { useEffect } from 'react';
 // React Native
 import {
+  Pressable,
+  Text,
   View
 } from 'react-native';
 // React Navigation
 import {
+  DrawerActions,
   type NavigationProp,
   useNavigation
 } from '@react-navigation/native';
@@ -17,6 +22,18 @@ import { globalStyles } from '../../theme';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect( () => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={ () => navigation.dispatch( DrawerActions.openDrawer() ) }
+        >
+          <Text>Menu</Text>
+        </Pressable>
+      )
+    });
+  }, [] );
 
   return (
     <View style={ globalStyles.container }>
