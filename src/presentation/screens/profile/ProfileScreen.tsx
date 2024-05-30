dd1997ad38ed108f9e44f07967d8893ea769fea3
Camilo López <camiloj.lopez79@gmail.com>
2024-05-30 11:56:34 -0400

    useSafeAreaInserts

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -3,12 +3,32 @@ import {
   Text,
   View
 } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import {
+  DrawerActions,
+  useNavigation
+} from '@react-navigation/native';
+import { PrimaryButton } from '../../components';
 
 
 export const ProfileScreen = () => {
+  const navigation = useNavigation();
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 20,
+        marginTop: top + 10
+      }
+    }>
+      <Text style={{ marginBottom: 10 }}>ProfileScreen</Text>
+
+      <PrimaryButton
+        label='Abrir menu'
+        onPress= { () => navigation.dispatch( DrawerActions.toggleDrawer() ) }
+      />
     </View>
   );
 }
